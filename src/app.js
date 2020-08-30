ab24a56383f5751f70f2e2d9d226606cb5edcdb5
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-08-30 22:54:19 +0530

    Setting up Props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision'
+        const subtitle = 'Put Your Life in the Hands of a Computer'
+        const options = ['One', 'Two', 'Four']
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle} />
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOption />
             </div>
         )
@@ -15,8 +18,8 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put Your Life in the Hands of a Computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         )
     }
@@ -36,8 +39,7 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <p>Options Components Here</p>
-                <Option />
+                {this.props.options.map((option) => <Option key={option} optionText={option}/>)}
             </div>
         )
     }
@@ -47,7 +49,7 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                <p>Option goes here</p>
+                {this.props.optionText}
             </div>
         )
     }
