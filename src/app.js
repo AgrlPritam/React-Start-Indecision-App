a6dcbf15320580f70ae7187a893238b1b6c3e24b
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-09-01 16:03:22 +0530

    Converting few classes to stateless components

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 //props is a one way communication from parent to child and we use functions(hasDeleteOptions,handlePick) to reverse the flow from child to parent
+//stateless functional component
+// const User = (props) => {
+//     return (
+//         <div>
+//             <p>Name: {props.name}</p>
+//             <p>Age: {props.age}</p>
+//         </div>
+//     )
+// }
+// ReactDOM.render(<User name="Horns" age={24} />,document.getElementById('app'))
 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -59,53 +69,93 @@ class IndecisionApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        )
-    }
+//stateless component - Header
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </div>
+    )
+}
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.title}</h1>
+//                 <h2>{this.props.subtitle}</h2>
+//             </div>
+//         )
+//     }
+// }
+
+//Converting Previous Class based component to stateless component - Action
+const Action = (props) => {
+    return (
+        <div>
+            <button 
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}
+            >
+                What Should I do?
+            </button>
+        </div>
+    )
+}
+// class Action extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <button 
+//                 onClick={this.props.handlePick}
+//                 disabled={!this.props.hasOptions}
+//                 >
+
+//                     What Should I do?
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+
+//Stateless Component - Options
+const Options = (props) => {
+    return (
+        <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {props.options.map((option) => <Option key={option} optionText={option}/>)}               
+        </div>
+    )
 }
 
-class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button 
-                onClick={this.props.handlePick}
-                disabled={!this.props.hasOptions}
-                >
+// class Options extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//             <button onClick={this.props.handleDeleteOptions}>Remove All</button>
+//             {this.props.options.map((option) => <Option key={option} optionText={option}/>)}               
+//             </div>
+//         )
+//     }
+// }
 
-                    What Should I do?
-                </button>
-            </div>
-        )
-    }
+//Stateless Component
+const Option = (props) => {
+    return (
+        <div>
+            {props.optionText}
+        </div>
+    )
 }
-
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-            <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-            {this.props.options.map((option) => <Option key={option} optionText={option}/>)}               
-            </div>
-        )
-    }
-}
-
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.optionText}
-            </div>
-        )
-    }
-}
+// class Option extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.optionText}
+//             </div>
+//         )
+//     }
+// }
 
 class AddOption extends React.Component {
     constructor(props){
